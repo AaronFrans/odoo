b92c838791e718b4f345a59f1c357fde70abba9d
Aaron Frans <61042747+AaronFrans@users.noreply.github.com>
2025-01-22 13:07:54 +0100

    [ADD] finished setup of the tutorial estate module

---
+++ addons/estate_tutorial/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Estate Tutorial",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Aaron Frans",
+    'category': 'Category',
+    'description': """
+    The module made in the Odoo tutorial, see: https://www.odoo.com/documentation/16.0/developer/tutorials/getting_started/03_newapp.html
+    """,
+    'license': 'LGPL-3',
+    'application': True
+}
