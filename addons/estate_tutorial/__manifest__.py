921a7a27ecc8dc1fe9dd4543d4ee3b9e84c68710
Aaron Frans <61042747+AaronFrans@users.noreply.github.com>
2025-01-23 11:53:16 +0100

    [Imp] Added test actions to estate_tutorial module

--- addons/estate_tutorial/__manifest__.py
+++ addons/estate_tutorial/__manifest__.py
@@ -7,7 +7,10 @@
     'description': """
     The module made in the Odoo tutorial, see: https://www.odoo.com/documentation/16.0/developer/tutorials/getting_started/03_newapp.html
     """,
-    'data': ['security/ir.model.access.csv'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'],
     'license': 'LGPL-3',
     'application': True
 }
