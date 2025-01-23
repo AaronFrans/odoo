3d6aecf96cfc55aed6c2268939771c84669fd83a
Aaron Frans <61042747+AaronFrans@users.noreply.github.com>
2025-01-23 11:30:15 +0100

    [IMP] Added security rules to tutorial_estate module

--- addons/estate_tutorial/__manifest__.py
+++ addons/estate_tutorial/__manifest__.py
@@ -7,6 +7,7 @@
     'description': """
     The module made in the Odoo tutorial, see: https://www.odoo.com/documentation/16.0/developer/tutorials/getting_started/03_newapp.html
     """,
+    'data': ['security/ir.model.access.csv'],
     'license': 'LGPL-3',
     'application': True
 }
