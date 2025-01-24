7f4241ca98d09196775384ed4fc6cbfb85e41745
Aaron Frans <61042747+AaronFrans@users.noreply.github.com>
2025-01-24 10:22:26 +0100

    [IMP] added default and readonly attributes in estate_property

--- addons/estate_tutorial/models/estate_property.py
+++ addons/estate_tutorial/models/estate_property.py
@@ -1,4 +1,7 @@
 from odoo import models, fields
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -8,10 +11,10 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Name", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
-    date_availability = fields.Date(string="Available Starting")
+    date_availability = fields.Date(string="Available Starting", default = datetime.today() + relativedelta(months=3), copy = False)
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float(string="Selling Price")
-    bedrooms = fields.Integer(string="Number of Bedrooms")
+    selling_price = fields.Float(string="Selling Price", readonly = True, copy = False)
+    bedrooms = fields.Integer(string="Number of Bedrooms", default = 2)
     living_area = fields.Integer(string="Size of Living Area")
     facades = fields.Integer(string="Number of Facades")
     garage = fields.Boolean(string="Garage Available")
@@ -19,3 +22,4 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer(string="Garden size")
     garden_orientation= fields.Selection(string='Garden Orientation',
                                          selection=[('north', 'North'), ('east', 'East'), ('south', 'South'), ('west', 'West')],)
+    active = fields.Boolean(string="Active", default=True)
