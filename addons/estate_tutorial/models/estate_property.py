638c357fe3a9f0f87f1699c3ba13bb28748d12fe
Aaron Frans <61042747+AaronFrans@users.noreply.github.com>
2025-01-24 12:53:07 +0100

    [IMP] added filters and groups to estate_property views, clarified measurment for properties in estate_property

--- addons/estate_tutorial/models/estate_property.py
+++ addons/estate_tutorial/models/estate_property.py
@@ -15,11 +15,11 @@ class EstateProperty(models.Model):
     expected_price = fields.Float(string="Expected Price", required=True)
     selling_price = fields.Float(string="Selling Price", readonly = True, copy = False)
     bedrooms = fields.Integer(string="Number of Bedrooms", default = 2)
-    living_area = fields.Integer(string="Size of Living Area")
+    living_area = fields.Integer(string="Size of Living Area (sqm)")
     facades = fields.Integer(string="Number of Facades")
     garage = fields.Boolean(string="Garage Available")
     garden = fields.Boolean(string="Garden Available")
-    garden_area = fields.Integer(string="Garden size")
+    garden_area = fields.Integer(string="Garden size (sqm)")
     garden_orientation = fields.Selection(string='Garden Orientation',
                                           selection=[('north', 'North'), ('east', 'East'), ('south', 'South'), ('west', 'West')],)
     active = fields.Boolean(string="Active", default=True)
