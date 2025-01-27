db141a71eb963811b0822cfe82546e619cc5e6fa
Aaron Frans <61042747+AaronFrans@users.noreply.github.com>
2025-01-27 10:29:57 +0100

    [IMP] updated the digimon module with basic views and changed the category

--- addons/digimon/__manifest__.py
+++ addons/digimon/__manifest__.py
@@ -3,12 +3,14 @@
     'version': '1.0',
     'depends': ['base'],
     'author': "Aaron Frans",
-    'category': 'Category',
+    'category': 'Testing/Digimon',
     'description': """
     Module to display a list of digimon via the api at: https://digimon-api.vercel.app/?ref=public_apis&utm_medium=website
     """,
     'data': [
-        'security/ir.model.access.csv',],
+        'security/ir.model.access.csv',
+        'views/digimon_views.xml',
+        'views/digimon_menus.xml'],
     'license': 'LGPL-3',
     'application': True
 }
